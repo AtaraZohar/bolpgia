15551e3c1fedeb47e49151410edd8651a91a3a5e
AtaraZohar <44782480+AtaraZohar@users.noreply.github.com>
2019-05-02 11:30:12 +0300

    Delete calculator.cpp

--- calculator.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "calculate.hpp"
-#include <string>
-#include "iostream"
-using namespace std;
-
-string bullpgia::calculateBullAndPgia(string choose, string gusses){
-    return "";
-}
